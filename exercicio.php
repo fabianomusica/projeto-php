7aa59d67b4a8e1bb623faf9cd3c9fcb4333e8a07
fabianomusica <fabianomusica@gmail.com>
2026-08-18 19:11:02 -0300

    pagina exercicio

---
+++ exercicio.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">    
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="recursos\CSS\estilo.css">
+    <title>Exercício</title>
+</head>
+
+<body>
+    <header class="cabecalho">
+        <h1>Curso de PHP</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+    <main class="principal">
+        <div class = "conteudo">
+            
+        </div>
+    </main>
+    <footer class="rodape">
+        <?= date('Y'); ?>
+</body>
+</html>
