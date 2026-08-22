61b82b5a0b03ddbc7718067109b261c0923f6c15
fabianomusica <fabianomusica@gmail.com>
2026-08-22 16:02:25 -0300

    css na pagina do primeiro exercicio

--- exercicio.php
+++ exercicio.php
@@ -4,14 +4,20 @@
     <meta charset="UTF-8">    
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="recursos\CSS\estilo.css">
+    <link rel="stylesheet" href="recursos\CSS\exercicio.css">
     <title>Exercício</title>
 </head>
 
-<body>
+<body class='exercicio'>
     <header class="cabecalho">
         <h1>Curso de PHP</h1>
         <h2>Visualização do Exercício</h2>
     </header>
+
+    <nav class="navegacao">
+        <a href="#" class='verde'>Sem formatação</a>
+        <a href="index.php" class='vermelho'>Voltar</a>
+    </nav>
     <main class="principal">
         <div class = "conteudo">
             
